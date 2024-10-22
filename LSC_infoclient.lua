d8518a56324e6f9ba7d988f8608be0eb91242d42
Noa Bengtsson <noab@chalmers.se>
2024-10-22 20:02:56 +0200

    Chanhes

---
+++ LSC_infoclient.lua
@@ -0,0 +1,30 @@
+local serialization = require("serialization")
+local component = require("component")
+
+local gpu = component.gpu
+local modem = component.modem
+
+recive_port = 1100
+server_port = 1000
+
+modem.open(recive_port)
+
+server_address = ""
+
+void = nil
+message = ""
+
+while true do
+    modem.send(server_address, server_port, modem.address, recive_port)
+    err = event.pull(15, "modem_message", void, void, void, void, message)
+    if err == nil then
+        print("Message timeout")
+    end
+    message = serialization.unserialize(message)
+    print(message["eu_stored"])
+    print(message["output_Average"])
+    print(message["input_Average"])
+    print(message["capacity"])
+    os.wait(5)
+
+end
--- LSC_infoserver.lua
+++ LSC_infoserver.lua
@@ -2,11 +2,10 @@
 
 local serialization = require("serialization")
 local component = require("component")
+local event = require("event")
 
 local gpu = component.gpu
 local modem = component.modem
-
-
 
 
 -- Settings
@@ -17,7 +16,7 @@ local broadcast_port = 1000
 -- End settings
 
 if LSC_address == nil then -- if LSC address in undefined, pull a gt_machine. If more gt machines are connected, then the progam will probably not work.
-    LSC_address = component.list("gt_machine")
+    LSC_address = component.list("gt_machine")()
 end
 
 LSC = component.proxy(LSC_address)
@@ -40,14 +39,15 @@ message["capacity"] = LSC.getEUCapacity()
 message["capacityString"] = LSC.getEUCapacityString()
 
 
--- runtime
-
-while true do
+function sendMessage(event_name, localAddress, remoteAddress, port, distance, send_address, send_port)
     message["eu_stored"] = LSC.getEUStored()
     message["output_Average"] = LSC.getEUOutputAverage()
     message["input_Average"] = LSC.getEUInputAverage()
-    if modem.broadcast(broadcast_port, serialization.serialize(message)) ~= true then
-        print("Error sending message")
-    end
-    os.sleep(5)
+    modem.send(send_address, send_port, serialization.serialize(message))
 end
+
+modem.open(broadcast_port)
+event.listen("modem_message", sendMessage)
+
+
+
