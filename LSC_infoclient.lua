36064eddfad71f496f5cd0dafa4463f64608be2e
Noa Bengtsson <noab@chalmers.se>
2024-10-22 23:51:47 +0200

    impl graphics

--- LSC_infoclient.lua
+++ LSC_infoclient.lua
@@ -1,9 +1,11 @@
 local serialization = require("serialization")
 local component = require("component")
 local event = require("event")
+local colors = require("colors")
 
 local gpu = component.gpu
 local modem = component.modem
+
 
 local recive_port = 1100
 local server_port = 1000
@@ -16,6 +18,25 @@ local err
 
 local message = nil
 
+
+function draw_screen(totalCap_EU, stored_EU, average_output, average_input)
+    --For 80x25 res screen. Maybe maxe resolutoin independetn
+    local fullFrac = stored_EU/totalCap_EU
+    gpu.fill(10, 15, 60, 5, "|") -- Draws background of power bar
+    gpu.setForeground(colors.green, true)
+    gpu.fill(10, 15, 60*fullFrac, 5, "|") -- Draws filled part of power bar
+    gpu.setForeground(colors.white, true)
+    gpu.set(10, 3, string.format("Total Capacity: %i", stored_EU))
+    gpu.set(10, 4, string.format("Stored Capacity: %i", totalCap_EU))
+    gpu.set(10, 5, string.format("EU Input: %i", average_input))
+    gpu.set(10, 6, string.format("EU Output: %i", average_output))
+
+    gpu.set(10, 7, string.format("Net EU: %i", average_input-average_output))
+    
+end
+
+
+
 while true do
     modem.send(server_address, server_port, modem.address, recive_port)
 
@@ -25,9 +46,13 @@ while true do
         os.exit()
     end
     message = serialization.unserialize(message)
-    print(string.format("Stored EU: %i", message["eu_stored"]))
-    print(string.format("Average Output EU: %i", message["output_Average"]))
-    print(string.format("Average Input EU: %i", message["input_Average"]))
-    print(string.format("Total Capacity EU: %i", message["capacity"]))
+    draw_screen(message["capacity"], message["eu_stored"], message["output_Average"], message["input_Average"])
+    -- debug..
+    --print(string.format("Stored EU: %i", message["eu_stored"]))
+    --print(string.format("Average Output EU: %i", message["output_Average"]))
+    --print(string.format("Average Input EU: %i", message["input_Average"]))
+    --print(string.format("Total Capacity EU: %i", message["capacity"]))
+
+
     os.sleep(5)
 end
