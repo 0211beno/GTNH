1b62e707e81516500690aa92851713628e4ebb2a
Noa Bengtsson <noab@chalmers.se>
2024-10-22 22:28:45 +0200

    c

--- LSC_infoserver.lua
+++ LSC_infoserver.lua
@@ -8,38 +8,38 @@ local gpu = component.gpu
 local modem = component.modem
 
 
--- Settings
-
-local LSC_address = nil
-local broadcast_port = 1000
-
--- End settings
-
-if LSC_address == nil then -- if LSC address in undefined, pull a gt_machine. If more gt machines are connected, then the progam will probably not work.
-    LSC_address = component.list("gt_machine")()
-end
-
-LSC = component.proxy(LSC_address)
--- Functions
---  getEUCapacity()
---  getEUCapacityString() (String output)
---  getEUStored()
---  getEUOutputAverage()
---  getEUInputAverage()
---  getAverageElectricOutput()
---  getAverageElectricInput()
-
-
-local totalCapacity = LSC.getEUCapacity()
-
-
-local message = {}
-
-message["capacity"] = LSC.getEUCapacity()
-message["capacityString"] = LSC.getEUCapacityString()
-
-
 function sendMessage(event_name, localAddress, remoteAddress, port, distance, send_address, send_port)
+        
+    -- Settings
+
+    local LSC_address = nil
+    local server_port = 1000
+
+    -- End settings
+
+    if LSC_address == nil then -- if LSC address in undefined, pull a gt_machine. If more gt machines are connected, then the progam will probably not work.
+        LSC_address = component.list("gt_machine")()
+    end
+
+    LSC = component.proxy(LSC_address)
+    -- Functions
+    --  getEUCapacity()
+    --  getEUCapacityString() (String output)
+    --  getEUStored()
+    --  getEUOutputAverage()
+    --  getEUInputAverage()
+    --  getAverageElectricOutput()
+    --  getAverageElectricInput()
+
+
+    local totalCapacity = LSC.getEUCapacity()
+
+
+    local message = {}
+
+    message["capacity"] = LSC.getEUCapacity()
+    message["capacityString"] = LSC.getEUCapacityString()
+
     message["eu_stored"] = LSC.getEUStored()
     message["output_Average"] = LSC.getEUOutputAverage()
     message["input_Average"] = LSC.getEUInputAverage()
@@ -55,7 +55,7 @@ function sendMessage(event_name, localAddress, remoteAddress, port, distance, se
     modem.send(send_address, send_port, serialization.serialize(message))
 end
 
-modem.open(broadcast_port)
+modem.open(server_port)
 event.listen("modem_message", sendMessage)
 
 
