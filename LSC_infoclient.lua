1dc623e52e91612a106e7bba96305a00ebbb32a1
Noa Bengtsson <noab@chalmers.se>
2024-10-22 23:24:49 +0200

    c

--- LSC_infoclient.lua
+++ LSC_infoclient.lua
@@ -10,12 +10,8 @@ local server_port = 1000
 
 modem.open(recive_port)
 
-local server_address = "1047eb80-972f-4f7c-b6e5-47957a29b6a4"
+local server_address = "d861217c-b169-4b50-a80d-243e36590912"
 
-local arg1 = nil
-local arg2 = nil
-local arg3 = nil
-local arg4 = nil
 local err
 
 local message = nil
